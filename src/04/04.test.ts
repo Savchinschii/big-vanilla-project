8ceebbb82dae84193a6e053218ed3432740206c6
Savcinschi Egor <sasha_lagun1995@mail.ru>
2022-09-06 17:39:49 +0300

    filter

---
+++ src/04/04.test.ts
@@ -0,0 +1,50 @@
+test('should take old men older then 90', ()=>{
+    const ages = [18, 20, 22, 1, 100, 90, 14];
+
+    const oldAges = ages.filter(age => age > 90);
+
+    expect(oldAges.length).toBe(1);
+    expect(oldAges[0]).toBe(100)
+})
+
+test('should take courses chipper 160', ()=>{
+    const courses = [
+        {title: 'CSS', price: 110},
+        {title: 'JS', price: 200},
+        {title: 'React', price: 150}
+    ]
+
+    const chipCourses = courses.filter(courses => courses.price < 160);
+
+    expect(chipCourses.length).toBe(2);
+    expect(chipCourses[0].title).toBe('CSS')
+    expect(chipCourses[1].title).toBe('React')
+})
+
+test('get only completed tasks', ()=> {
+    const tasks = [
+        {id: 1, title: 'Bread', isDone: false},
+        {id: 2, title: 'Milk', isDone: true},
+        {id: 3, title: 'Solt', isDone: false},
+        {id: 4, title: 'Sugar', isDone: true}
+    ]
+
+    const completedTasks = tasks.filter(task => task.isDone)
+    expect(completedTasks.length).toBe(2);
+    expect(completedTasks[0].id).toBe(2)
+    expect(completedTasks[1].id).toBe(4)
+})
+
+test('get only uncompleted tasks', ()=> {
+    const tasks = [
+        {id: 1, title: 'Bread', isDone: false},
+        {id: 2, title: 'Milk', isDone: true},
+        {id: 3, title: 'Solt', isDone: false},
+        {id: 4, title: 'Sugar', isDone: true}
+    ]
+
+    const unCompletedTasks = tasks.filter(task => !task.isDone)
+    expect(unCompletedTasks.length).toBe(2);
+    expect(unCompletedTasks[0].id).toBe(1)
+    expect(unCompletedTasks[1].id).toBe(3)
+})
---
+++ src/04/04.ts
@@ -0,0 +1,29 @@
+const ages = [18, 20, 22, 1, 100, 90, 14];
+
+const predicate = (age: number) => {
+    return age > 90;
+}
+
+const oldAges = [100];
+
+type CourseType = {
+    title: string
+    price: number
+}
+
+const courses = [
+    {title: 'CSS', price: 110},
+    {title: 'JS', price: 200},
+    {title: 'React', price: 150}
+]
+
+// < 160
+const chipPredicate = (courses: CourseType) => {
+    return courses.price < 160;
+}
+
+const chipCourses = [
+    {title: 'CSS', price: 110},
+    {title: 'React', price: 150}
+]
+
